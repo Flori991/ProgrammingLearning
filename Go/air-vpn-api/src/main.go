4191a82ed16b9400ac8a70e3b76f2e2497bf485b
Flori991 <santico.fugenkratzer@gmail.com>
2026-03-28 20:28:19 +0000

    Introduce proper logger and log levels

--- Go/air-vpn-api/src/main.go
+++ Go/air-vpn-api/src/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"encoding/json"
 	"io"
-	"log"
 	"net/http"
 	"strings"
 
@@ -11,20 +10,25 @@ import (
 )
 
 func main() {
+	// Initialize logger
+	initLogLevel()
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/dashboard", handleDashboardData)
 
-	log.Println("Starting server on port: " + SERVER_PORT)
+	logInfo("Starting server on port: " + SERVER_PORT)
 	err := http.ListenAndServe(":"+SERVER_PORT, mux)
 	if err != nil {
-		log.Println("Server failed to start:", err)
+		logError("Server failed to start:", err)
 	}
 }
 
 func handleDashboardData(w http.ResponseWriter, r *http.Request) {
+	logInfo("Received request for dashboard data.")
 	// Get API key from request header
 	API_KEY := r.Header.Get("API-KEY")
 	if API_KEY == "" {
+		logError("API key is missing")
 		w.Header().Set("x-missing-field", "API-KEY")
 		http.Error(w, "API key is missing", http.StatusBadRequest)
 		return
@@ -33,11 +37,12 @@ func handleDashboardData(w http.ResponseWriter, r *http.Request) {
 	// Fetch UserInfo
 	userInfoResponseBytes, err := httpGet(AIRVPN_USERINFO_URL, API_KEY)
 	if err != nil {
-		log.Println("Failed to fetch user info:", err)
+		logError("Failed to fetch user info:", err)
 		http.Error(w, "Failed to fetch user info", http.StatusInternalServerError)
 		return
 	}
 	if strings.Contains(string(userInfoResponseBytes), "Not authorized") {
+		logWarning("API key is not valid")
 		http.Error(w, "API key is not valid", http.StatusUnauthorized)
 		return
 	}
@@ -45,7 +50,7 @@ func handleDashboardData(w http.ResponseWriter, r *http.Request) {
 	//Fetch Server Status
 	statusResponseBytes, err := httpGet(AIRVPN_STATUS_URL)
 	if err != nil {
-		log.Println("Failed to fetch server status:", err)
+		logError("Failed to fetch server status:", err)
 		http.Error(w, "Failed to fetch server status", http.StatusInternalServerError)
 		return
 	}
@@ -53,12 +58,12 @@ func handleDashboardData(w http.ResponseWriter, r *http.Request) {
 	// Turn userinfo json response into response type
 	userInfo, err := safeJsonParse(userInfoResponseBytes, types.UserInfo{})
 	if err != nil {
-		log.Println("Failed to parse user info response:", err)
+		logError("Failed to parse user info response:", err)
 		http.Error(w, "Failed to parse user info response", http.StatusInternalServerError)
 		return
 	}
 	if len(userInfo.Sessions) == 0 {
-		log.Println("No active sessions found.")
+		logWarning("No active sessions found.")
 		http.Error(w, "No active sessions found", http.StatusInternalServerError)
 		return
 	}
@@ -66,12 +71,12 @@ func handleDashboardData(w http.ResponseWriter, r *http.Request) {
 	// Turn status json response into response type
 	status, err := safeJsonParse(statusResponseBytes, types.Status{})
 	if err != nil {
-		log.Println("Failed to parse server status response:", err)
+		logError("Failed to parse server status response:", err)
 		http.Error(w, "Failed to parse server status response", http.StatusInternalServerError)
 		return
 	}
 	if len(status.Servers) == 0 {
-		log.Println("No server status information found.")
+		logWarning("No server status information found.")
 		http.Error(w, "No server status information found", http.StatusInternalServerError)
 		return
 	}
@@ -82,7 +87,7 @@ func handleDashboardData(w http.ResponseWriter, r *http.Request) {
 	// Serialize for return
 	jsonSessionSummaries, err := json.Marshal(sessionSummaries)
 	if err != nil {
-		log.Println("Failed to serialize response:", err)
+		logError("Failed to serialize response:", err)
 		http.Error(w, "Failed to serialize response", http.StatusInternalServerError)
 		return
 	}
