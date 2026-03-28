7a234f402af7b2aec0e4440d971ea8a3ada4d6c4
Flori991 <santico.fugenkratzer@gmail.com>
2026-03-28 14:30:50 +0000

    Use new request file

--- Go/air-vpn-api/src/main.go
+++ Go/air-vpn-api/src/main.go
@@ -2,26 +2,25 @@ package main
 
 import (
 	"encoding/json"
-	"io"
 	"log"
-	"net/http"
-	"os"
 	"strings"
 
 	types "github.com/Flori991/ProgrammingLearning/types"
 )
 
-const API_URL = "https://airvpn.org/api/userinfo/?key="
-const ENV_API_KEY = "AIRVPN_API_KEY"
-
 func main() {
-	// Do the request and return json response
-	responsebytes := sendGetRequest()
-	if strings.Contains(string(responsebytes), "Not authorized") {
+	// Fetch UserInfo
+	userInfoResponseBytes := sendGetRequest(AIRVPN_USERINFO_URL, ENV_API_KEY)
+	if strings.Contains(string(userInfoResponseBytes), "Not authorized") {
 		log.Fatal("API key is not valid. Please check your environment variable.")
 	}
+
+	//Fetch Server Status
+	statusResponseBytes := sendGetRequest(AIRVPN_STATUS_URL)
+	log.Println("Server status response:", string(statusResponseBytes))
+
 	// Turn json response intro response type
-	userInfo := parseResponse(responsebytes)
+	userInfo := parseResponse(userInfoResponseBytes)
 	if len(userInfo.Sessions) == 0 {
 		log.Println("No active sessions found.")
 	}
@@ -29,20 +28,6 @@ func main() {
 	jsonSessionSummaries := serializeResponse(userInfo.Sessions)
 
 	log.Println("API call successful. Response:", jsonSessionSummaries)
-}
-
-func sendGetRequest() []byte {
-	log.Println("Starting API call...")
-	resp, err := http.Get(API_URL + os.Getenv(ENV_API_KEY))
-
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer resp.Body.Close()
-
-	log.Println("Response status:", resp.Status)
-	body, err := io.ReadAll(resp.Body)
-	return body
 }
 
 func parseResponse(body []byte) types.UserInfo {
