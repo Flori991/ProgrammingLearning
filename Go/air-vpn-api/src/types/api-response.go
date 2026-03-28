407efd412944c9cff5e1fe9348ccc816be83a17e
Flori991 <santico.fugenkratzer@gmail.com>
2026-03-28 13:47:08 +0000

    Add more field to session summary and add array struct

--- Go/air-vpn-api/src/types/api-response.go
+++ Go/air-vpn-api/src/types/api-response.go
@@ -1,8 +1,19 @@
 package types
 
+import "encoding/json"
+
 type SessionSummary struct {
-	DeviceName         string `json:"device_name"`
-	DeviceDescription  string `json:"device_description"`
-	ConnectedSinceDate string `json:"connected_since_date"`
-	ExitIp             string `json:"exit_ip"`
+	DeviceName         string      `json:"device_name"`
+	DeviceDescription  string      `json:"device_description"`
+	EntryIpv4          string      `json:"entry_ipv4"`
+	ServerName         string      `json:"server_name"`
+	ServerCountry      string      `json:"server_country"`
+	BytesRead          json.Number `json:"bytes_read"`
+	BytesWrite         json.Number `json:"bytes_write"`
+	ConnectedSinceDate string      `json:"connected_since_date"`
+	ConnectedSinceUnix int         `json:"connected_since_unix"`
+}
+
+type SessionSummaries struct {
+	Sessions []SessionSummary `json:"sessions"`
 }
