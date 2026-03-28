b4f5f6b1806b790f9d7ec010df46f6670d96011d
Flori991 <santico.fugenkratzer@gmail.com>
2026-03-28 15:52:48 +0000

    Add utils file

---
+++ Go/air-vpn-api/src/utils.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"slices"
+
+	"github.com/Flori991/ProgrammingLearning/types"
+)
+
+func mergeResponsesIntoSummaries(sessions []types.Session, status []types.ServerStatus) types.SessionSummaries {
+	var sessionSummaries []types.SessionSummary
+	for _, session := range sessions {
+
+		index := slices.IndexFunc(status, func(s types.ServerStatus) bool { return s.ServerName == session.ServerName })
+		matchingServerStatus := status[index]
+
+		sessionSummaries = append(sessionSummaries, types.SessionSummary{
+			DeviceName:         session.DeviceName,
+			DeviceDescription:  session.DeviceDescription,
+			ExitIpv4:           session.ExitIpv4,
+			ServerName:         session.ServerName,
+			ServerCountry:      session.ServerCountry,
+			BandwidthUsed:      matchingServerStatus.BandwidthUsed,
+			BandwidthMax:       matchingServerStatus.BandwidthMax,
+			BytesRead:          session.BytesRead,
+			BytesWrite:         session.BytesWrite,
+			ConnectedSinceDate: session.ConnectedSinceDate,
+			ConnectedSinceUnix: session.ConnectedSinceUnix,
+		})
+	}
+	return types.SessionSummaries{
+		Sessions: sessionSummaries,
+	}
+}
+
+func safeJsonParse[T any](body []byte, target T) T {
+	if err := json.Unmarshal(body, &target); err != nil {
+		log.Fatal("Something went wrong parsing a json response:", err)
+	}
+	return target
+}
