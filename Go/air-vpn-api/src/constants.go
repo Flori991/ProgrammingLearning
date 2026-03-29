45202f78a74e07a082e06ac76b8dc08ecb0589ef
Flori991 <santico.fugenkratzer@gmail.com>
2026-03-29 09:10:47 +0100

    Move env var name into constants

--- Go/air-vpn-api/src/constants.go
+++ Go/air-vpn-api/src/constants.go
@@ -2,6 +2,7 @@ package main
 
 const AIRVPN_USERINFO_URL = "https://airvpn.org/api/userinfo/?key="
 const AIRVPN_STATUS_URL = "https://airvpn.org/api/status/"
+const ENV_LOG_LEVEL = "LOG_LEVEL"
 const SERVER_PORT = "3000"
 
 const LOG_ERROR = "[ERROR] "
